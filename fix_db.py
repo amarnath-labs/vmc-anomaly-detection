bf3e4fb7207aebb7e4f07d0befbc180e547b5312
Amarnath <amarnathgolden123@gmail.com>
2026-05-10 01:29:17 +0530

    updated vmc worker (pause photo)

---
+++ fix_db.py
@@ -0,0 +1,14 @@
+﻿import sqlite3
+
+con = sqlite3.connect("vmc_readings.db")
+cols = [row[1] for row in con.execute("PRAGMA table_info(readings)")]
+
+if "anomaly_type" not in cols:
+    con.execute("ALTER TABLE readings ADD COLUMN anomaly_type TEXT")
+    print("Added anomaly_type column")
+else:
+    print("anomaly_type column already exists")
+
+con.commit()
+con.close()
+print("DB fixed")
